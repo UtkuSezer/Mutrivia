c39320781966b4c5fa383dd1b7cf1bc81f26574a
Darnque <s.utkusezer@hotmail.com>
2023-04-24 16:33:21 +0300

    Icons added, Navbar organized

--- frontend/mutrivia-flutter/lib/main.dart
+++ frontend/mutrivia-flutter/lib/main.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Mutrivia'),
-      centerTitle: true,
-      backgroundColor: Colors.deepPurple[500],
-    ),
-
-    body: Center(
-      child: Text('Hello World'),
-    ),
-
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text(
-          'Submit',
-        style: TextStyle(
-          fontSize: 12.0
-        ),
-      ),
-      backgroundColor: Colors.deepPurple[500],
-    ),
-  ),
-
-  theme: ThemeData(
-    fontFamily: 'Nunito'
-  ),
+  home: Body(),
+  theme: ThemeData(fontFamily: 'Nunito'),
 ));
 
+class Body extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center, // Center Row contents horizontally
+          children: <Widget>[
+            Image.asset('assets/images/favicon_white.png', height: 40, width: 40,),
+            const Text('Mutrivia'),
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple[500],
+      ),
+
+      body: const Center(
+        child: Icon(
+          Icons.account_balance_outlined,
+          size: 150.0,
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple[500],
+        child: const Text(
+          'Submit',
+          style: TextStyle(
+              fontSize: 12.0
+          ),
+        ),
+      ),
+    );
+  }
+}
