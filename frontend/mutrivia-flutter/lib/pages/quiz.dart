2613df452ce05e98b017a548ce0f7f1ae5ac7d57
Darnque <s.utkusezer@hotmail.com>
2023-04-24 23:29:18 +0300

    Username screen added

---
+++ frontend/mutrivia-flutter/lib/pages/quiz.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Quiz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mutrivia',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Nunito',
+      ),
+    );
+  }
+}
