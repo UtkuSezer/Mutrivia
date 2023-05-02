755d7bd5967c841111dd8c40ef398221504f7df3
Gökhan Taş <tasgokhan389@hotmail.com>
2023-05-02 18:36:25 +0300

    Update app.constants.ts

--- frontend/mutrivia-web/src/app/app.constants.ts
+++ frontend/mutrivia-web/src/app/app.constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://wwww.mutrivia.com/api"
-export const USER_API_URL = "http://wwww.mutrivia.com/api/user"
-export const GAME_API_URL = "http://wwww.mutrivia.com/api/game"
-export const ADMIN_API_URL = "http://wwww.mutrivia.com/api/admin"
+export const API_URL = "http://www.mutrivia.com/api"
+export const USER_API_URL = "http://www.mutrivia.com/api/user"
+export const GAME_API_URL = "http://www.mutrivia.com/api/game"
+export const ADMIN_API_URL = "http://www.mutrivia.com/api/admin"
