ffccf919f5898a12ead990736c3dd959c2a541e5
Darnque <s.utkusezer@hotmail.com>
2023-05-21 22:54:02 +0300

    Finalized, ready to demo deploy

--- frontend/mutrivia-web/src/app/app.component.ts
+++ frontend/mutrivia-web/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { GameDataService } from './service/game-data.service'
+import { UserDataService } from './service/user-data.service'
+import { getLocaleTimeFormat } from '@angular/common';
 
 export let browserRefresh = false;
 
@@ -11,10 +14,73 @@ export let browserRefresh = false;
 })
 export class AppComponent {
   title = 'mutrivia-web';
+  fullLoader = false
 
   subscription: Subscription | undefined;
 
-  constructor(private router: Router) {
+  @HostListener('window:unload', [ '$event' ])
+  unloadHandler(event: any) {
+    // ...
+
+    
+  }
+
+  @HostListener('window:beforeunload', [ '$event' ])
+  beforeUnloadHandler(event: any) {
+    // ...
+    this.fullLoader = true
+    if(sessionStorage.getItem('userId') as string == null){
+      this.fullLoader = false
+      return true;
+    }
+    else{
+      console.log("User To Delete: " + sessionStorage.getItem('userId') as string)  
+      if(sessionStorage.getItem('isHost') == "true"){
+        this.gameDataService.switchToResults(sessionStorage.getItem('userId') as string).subscribe(
+          data => {
+            this.gameDataService.endSession(sessionStorage.getItem('userId') as string).subscribe(
+              data => {
+                this.userDataService.deleteUser(sessionStorage.getItem('userId') as string).subscribe(
+                  data => {
+                     sessionStorage.removeItem("userId");
+                     this.fullLoader = false
+                     this.router.navigate(['register']);
+                  }
+                )
+              }
+            )
+          }
+        )
+      }
+      else if(sessionStorage.getItem('isParticipant') == "true"){
+        this.gameDataService.leaveSession(sessionStorage.getItem('userId') as string).subscribe(
+          data => {
+            this.userDataService.deleteUser(sessionStorage.getItem('userId') as string).subscribe(
+              data => {
+                 sessionStorage.removeItem("userId");
+                 this.fullLoader = false
+                 this.router.navigate(['register']);
+              }
+            )
+          }
+        )
+      }
+      else{
+        this.userDataService.deleteUser(sessionStorage.getItem('userId') as string).subscribe(
+          data => {
+             sessionStorage.removeItem("userId");
+             this.fullLoader = false
+             this.router.navigate(['register']);
+          }
+        )
+      }
+      return false;
+    }
+
+    
+  }
+
+  constructor(private router: Router, private userDataService: UserDataService, private gameDataService: GameDataService) {
     this.subscription = router.events.subscribe((event) => {
         if (event instanceof NavigationStart) {
           browserRefresh = !router.navigated;
